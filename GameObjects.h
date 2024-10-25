bf31265c8bb47ca80a8d64ee8a632b2cbed6e8d1
masha_sit <mariyadance2004@gmail.com>
2024-10-25 12:51:54 +0300

    project files

---
+++ GameObjects.h
@@ -0,0 +1,46 @@
+#ifndef GAMEOBJECTS_H
+#define GAMEOBJECTS_H
+
+enum ShapeType
+{
+    REULE,
+    SQUARE,
+    TRIANGLE,
+    RHOMBUS,
+    BOMB,
+    FILL,
+    ELLIPSE,
+    RETURN,
+    GEMStext
+};
+
+enum Color
+{
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    MAGENTA,
+    CYAN,
+    BROWN,
+    GRAY,
+    PINK,
+    ORANGE,
+    BLACK,
+    WHITE
+};
+
+struct GameObject
+{
+    int x;
+    int y;
+    Color color;
+    ShapeType shapeType;
+
+    bool operator==(const GameObject& other) const
+    {
+        return (x == other.x && y == other.y);
+    }
+};
+
+#endif
